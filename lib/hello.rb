5895034150ffdb14bd60e0f553ebc6fcbb45bdab
Jamil Seddiqi <jamilsediq@gmail.com>
2020-02-18 03:27:52 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,16 +1,20 @@
 # def hello_t
 
-def hello_t(array)
-    i = 0
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
-    array
+# def hello_t(array)
+#     i = 0
+#     while i < array.length
+#       yield(array[i])
+#       i = i + 1
+#     end
+#     array
+# end
+
+
+
+def yield_example
+	puts "Hi I'm having a nice day."
+	yield if block_given?
+	puts "How did you get here?!"
 end
-
-
-
-
 # call your method here!
 
